f5323189520fb4a7d1edc65387e38a6e41b2939a
Sebastian Kapunkt <SebastianKapunkt@users.noreply.github.com>
2018-03-09 17:36:12 +0100

    Add function to get a receiver that didn't received a feedback yet (#18)
    * Add function to get a receiver that didn't received a feedback yet
    
    * fix wrong list comprehension
    
    * Fix a couple of things

--- stanley/helpers.py
+++ stanley/helpers.py
@@ -1,14 +1,15 @@
 import secrets
 
 from stanley.redis import redis_storage
-from stanley.settings import FEEDBACK_MEMBERS, REDIS_KEY_SEND_FEEDBACK
+from stanley.settings import FEEDBACK_MEMBERS, REDIS_KEY_RECEIVE_FEEDBACK, REDIS_KEY_SEND_FEEDBACK
 from stanley.slack import get_team_members, send_slack_message
 
 
 def request_feedback():
     """Ask random user to give a feedback for another random user."""
     members = get_filtered_member()
-    sender, receiver = get_sender_receiver(members)
+    sender = get_sender(members)
+    receiver = get_receiver(members, sender)
 
     # set sender, receiver pair in the storage
     redis_storage.set(sender[0], receiver[0])
@@ -20,11 +21,11 @@ def get_filtered_member():
     members = get_team_members()
     if FEEDBACK_MEMBERS:
         # if we have variable set, ignore other people
-        members = [member for member in members if member[1] in FEEDBACK_MEMBERS]
+        members = [m for m in members if m[1] in FEEDBACK_MEMBERS]
     return members
 
 
-def get_sender_receiver(members):
+def get_sender(members):
     # list of user that send a feedback already
     sent_already = redis_storage.smembers(REDIS_KEY_SEND_FEEDBACK)
 
@@ -35,15 +36,29 @@ def get_sender_receiver(members):
         sent_already = []
 
     # subtract the list of people that already have send from the member
-    can_send = [member for member in members if member[0] not in sent_already]
+    can_send = [m for m in members if m[0] not in sent_already]
     random_sender = secrets.choice(can_send)
     # put the picked sender as key to list so we don't bother him until
     # everyone else was asked to provide a feedback
     redis_storage.sadd(REDIS_KEY_SEND_FEEDBACK, random_sender[0])
 
-    # figure out to who we can send the feedback, basically just not to
-    # the person we picked to give a feedback
-    can_receive = [member for member in members if member is not random_sender]
-    random_receiver = secrets.choice(can_receive)
+    return random_sender
 
-    return random_sender, random_receiver
+
+def get_receiver(members, sender):
+    # subtract the list of people that already have received a feedback and
+    # also remove the person that is the sender
+    received_already = redis_storage.smembers(REDIS_KEY_RECEIVE_FEEDBACK)
+    can_receive = [m for m in members if m[0] not in received_already and m is not sender]
+
+    # if the size of the list is zero, everyone received a feedback and we can start again
+    if len(can_receive) == 0:
+        redis_storage.delete(REDIS_KEY_SEND_FEEDBACK)
+        can_receive = [m for m in members if m is not sender]
+
+    random_receiver = secrets.choice(can_receive)
+    # put the picked receiver as key to list so we don't bother him until
+    # everyone else received a feedback
+    redis_storage.sadd(REDIS_KEY_RECEIVE_FEEDBACK,  random_receiver[0])
+
+    return random_receiver
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,11 @@
-from stanley.helpers import get_sender_receiver
+from stanley.helpers import get_receiver, get_sender
 from stanley.redis import redis_storage
-from stanley.settings import REDIS_KEY_SEND_FEEDBACK
+from stanley.settings import REDIS_KEY_RECEIVE_FEEDBACK, REDIS_KEY_SEND_FEEDBACK
 
 
-def test_get_sender_receiver(members):
-    sender, receiver = get_sender_receiver(members)
+def test_get_sender(members):
+    sender = get_sender(members)
 
-    assert sender is not receiver
     saved_sender = redis_storage.smembers(REDIS_KEY_SEND_FEEDBACK)
     assert sender[0] in saved_sender
 
@@ -14,19 +13,18 @@ def test_get_sender_receiver(members):
     redis_storage.delete(REDIS_KEY_SEND_FEEDBACK)
 
 
-def test_get_sender_receiver_when_already_run(members):
+def test_get_sender_when_already_run(members):
     amureki_id = members[0][0]
     sebastian_id = members[1][0]
 
     # let's say that amureki already provided a feedback
     redis_storage.sadd(REDIS_KEY_SEND_FEEDBACK, amureki_id)
 
-    sender, receiver = get_sender_receiver(members)
+    sender = get_sender(members)
 
-    # we have just two members and one is already answered so
-    # we know who will be the sender and the receiver
+    # we have just two members and one has already answered so
+    # we know who will be the sender
     assert sender[0], sebastian_id
-    assert receiver[0], amureki_id
 
     saved_sender = redis_storage.smembers(REDIS_KEY_SEND_FEEDBACK)
     assert amureki_id in saved_sender
@@ -34,3 +32,48 @@ def test_get_sender_receiver_when_already_run(members):
 
     # cleanup used key
     redis_storage.delete(REDIS_KEY_SEND_FEEDBACK)
+
+
+def test_get_receiver(members):
+    amureki = members[0]
+    sebastian = members[1]
+
+    # say sebastian is the sender so we expect amureki
+    # to be the receiver
+    receiver = get_receiver(members, sebastian)
+
+    assert receiver, amureki
+
+    # cleanup used key
+    redis_storage.delete(REDIS_KEY_RECEIVE_FEEDBACK)
+
+
+def test_get_receiver_impossible(members):
+    """Handle edge case.
+
+    It is possible that the last person that is left to get feedback
+    is the same person as the sender. The function should handle this
+    case and return a proper receiver.
+
+    """
+    amureki = members[0]
+    sebastian = members[1]
+
+    # let's say that amureki already received feedback
+    # that means that only sebastian is left as receiver of a feedback
+    redis_storage.sadd(REDIS_KEY_RECEIVE_FEEDBACK, amureki[0])
+
+    # here we decide that sebastian is the sender, so he can't be
+    # the receiver
+    receiver = get_receiver(members, sebastian)
+
+    saved_receiver = redis_storage.smembers(REDIS_KEY_RECEIVE_FEEDBACK)
+
+    # since sebastian is the sender, we expect that amureki is the receiver
+    assert amureki[0] in saved_receiver
+    # sebastian should not have received a feedback yet
+    assert sebastian[0] not in saved_receiver
+    assert receiver, amureki
+
+    # cleanup used key
+    redis_storage.delete(REDIS_KEY_RECEIVE_FEEDBACK)
