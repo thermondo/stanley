5330258f164cfc79cef12cb0c65c93888a143c3f
Denis Cornehl <denis@cornehl.org>
2021-11-16 10:19:45 +0100

    fix error after upgrade. Default encoding in redis-py is utf-8 anyways

--- stanley/redis.py
+++ stanley/redis.py
@@ -2,8 +2,4 @@ import redis as redis
 
 from stanley.settings import REDIS_URL
 
-redis_storage = redis.from_url(
-    url=REDIS_URL,
-    charset='utf-8',
-    decode_responses=True
-)
+redis_storage = redis.from_url(url=REDIS_URL, decode_responses=True)
