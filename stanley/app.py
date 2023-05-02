aa28d9a85dfaea61d23432c07993b84a170ec5b6
Sudo'er <59538442+5udu3r@users.noreply.github.com>
2023-05-02 12:46:11 +0330

    replace flask with fastapi

--- stanley/app.py
+++ stanley/app.py
@@ -1,18 +1,18 @@
 import click
 import sentry_sdk
-from flask import Flask
-from sentry_sdk.integrations.flask import FlaskIntegration
+from fastapi import FastAPI
+from sentry_sdk.integrations.asgi import SentryIntegration
 
 from stanley.helpers import request_feedback
 from stanley.settings import SENTRY_DSN
 from stanley.slack import get_team_members
 
-sentry_sdk.init(  # type: ignore
+sentry_sdk.init(
     dsn=SENTRY_DSN,
-    integrations=[FlaskIntegration()]
+    integrations=[SentryIntegration()]
 )
 
-app = Flask(__name__)
+app = FastAPI()
 
 import stanley.routes  # NoQA # isort:skip # pylint: disable=unused-import
 assert stanley.routes  # nosec
