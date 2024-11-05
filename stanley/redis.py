099f83559a38158335681669c39c7edcf6ce6377
Denis Cornehl <denis@cornehl.org>
2024-11-05 15:11:01 +0100

    fix tests

--- stanley/redis.py
+++ stanley/redis.py
@@ -2,8 +2,15 @@ import redis as redis
 
 from stanley.settings import REDIS_URL
 
+if "rediss://" in REDIS_URL:
+    kwargs = dict(
+        ssl_cert_reqs=None,
+    )
+else:
+    kwargs = dict()
+
 redis_storage = redis.from_url(
     url=REDIS_URL,
     decode_responses=True,
-    ssl_cert_reqs=None,
+    **kwargs,
 )
